a17254fea539b87b1798b32f396a74d6496a121c
dsinecos <dsinecos@gmail.com>
2017-02-01 18:24:24 +0530

    Adds icons corresponding to weather and toggles temperature between Celsius and Fahrenheit

--- fccIntermediateProjects/weatherApp/weatherApp.js
+++ fccIntermediateProjects/weatherApp/weatherApp.js
@@ -7,31 +7,76 @@ $(document).ready(function weatherData() {
 
 		navigator.geolocation.getCurrentPosition(function(position){
 
-				$latitude = position.coords.latitude;
-				$longitude = position.coords.longitude;
+			$latitude = position.coords.latitude;
+			$longitude = position.coords.longitude;
 
-				$.getJSON('https://geoip-db.com/json/geoip.php?jsonp=?') 
-				.done (function(location)
-				{
+			$.getJSON('https://geoip-db.com/json/geoip.php?jsonp=?') 
+			.done (function(location)
+			{
 
-					$('.location').append(location.country_name + ", ");
-					$('.location').append(location.state + ", ");
-					$('.location').append(location.city);
+				$('.location').append(location.country_name + ", ");
+				$('.location').append(location.state + ", ");
+				$('.location').append(location.city);
 
-				});				
+			});				
 
-				$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + $latitude + "&lon=" + $longitude + "&APPID=71323636795d62e17fc6101b104d40f0", function(json){
-					
-					//console.log(json);
+			$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + $latitude + "&lon=" + $longitude + "&APPID=71323636795d62e17fc6101b104d40f0", function(json){
 
-					String.prototype.capitalize = function() {
-						return this.charAt(0).toUpperCase() + this.slice(1);
+				String.prototype.toCapital = function() {
+					return this.charAt(0).toUpperCase() + this.slice(1);
+				}
+
+				$(".temperature .temperatureValue").prepend(Math.round(json.main.temp - 275.16) + " ");
+				$(".weatherDescription").append(json.weather[0].description.toCapital());
+				$(".forIcon").append("<img src=" + "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png>");					
+
+			});
+
+			$(".toggleTemperature").click(switchTemperature);
+
+			function switchTemperature() {
+
+				var tempClass = document.getElementsByClassName("temperature");
+				// .getAttribute("data-temp_scale");
+
+				var tempScale = $(".temperature").attr("data-tempscale");
+				console.log(tempScale);
+
+				// msglist.setAttribute("data-list-size", +show+3);				
+
+				// var tempScale = $(".temperature").data("temp_scale");
+
+					// console.log($(".temperature").data("temp_scale"))
+
+					if (tempScale == 'in_celsius') {
+
+						var celsius = parseInt($(".temperature .temperatureValue").text());
+						var toFahrenheit = (celsius * 9) /5 + 32;
+
+						$(".temperature").attr("data-tempscale", "in_fahrenheit");
+						$(".temperature .temperatureValue").text(toFahrenheit + " ");
+
+						console.log("Before fahrenheit worked");
+						$(".toggleTemperature").text("Degree Fahrenheit");
+						console.log("After fahrenheit worked");
+
+					} else if (tempScale == 'in_fahrenheit') {
+
+						var fahrenheit = parseInt($(".temperature .temperatureValue").text());
+						var toCelsius = Math.round((fahrenheit - 32) * 5 / 9);
+
+						$(".temperature").attr("data-tempscale", "in_celsius");
+						$(".temperature .temperatureValue").text(toCelsius + " ");
+						$(".toggleTemperature").text("Degree Celsius");
 					}
 
-					$(".temperature").append(Math.round(json.main.temp - 275.16) + "Degree Celsius");
-					$(".weatherDescription").append(json.weather[0].description.capitalize());					
+				/*					
+					var celsius = parseInt($(".temperature .temperatureValue").text())
+					var toFahrenheit = (celsius * 9) /5 + 32
+					$(".temperature .temperatureValue").text(toFahrenheit + " ");
+					*/
 
-				});
+				}
 			});
 	} 
 
