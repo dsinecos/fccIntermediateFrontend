7df3ff18af287dee74b76f2e36c203c1bb5baa30
dsinecos <dsinecos@gmail.com>
2017-02-20 10:54:51 +0530

    Understanding what the prototypes point to incase of a function constructor and in case of Object.create

--- experiments/weirdJavaScript.js
+++ experiments/weirdJavaScript.js
@@ -1,16 +1,45 @@
+
 function person() {
 
-	name = 'name';
-	fullname = 'fullname';
-	getname = function(){};
+	this.name = 'name';
+	this.fullname = 'fullname';
+	this.getname = function(){};
 
 }
 
-console.log(typeof person.prototype);
+/*
+person.prototype.setname = function(){
+	var test ='setname';
+	console.log("Does this work ");
+}
+*/
+
+var demo = new person();
+
+
 console.log(person.prototype);
 
-var demo = Object.create(person.prototype);
+/*
+console.log(demo);
+console.log("........................");
+console.log(demo.__proto__);
+console.log("........................");
+console.log(demo.setname);
+console.log(demo.setname());
+*/
+/*
+
+var person = {
+	name: 'name',
+	fullname: 'fullname',
+	getname: function() {}
+}
+
+var demo = Object.create(person);
+
 
 console.log(demo);
-
-// console.log(person.prototype.name);
+console.log("........................");
+console.log(demo.__proto__);
+console.log("........................");
+*/
